1b8ac6cb40039ee5a6a0a5a2ca86a79754d89307
root <root@BananaTop.localdomain>
2026-07-30 16:21:02 -0400

    feat: provision regional private cloud smoke infrastructure

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,6 +1,11 @@
 terraform {
   required_version = "~> 1.15"
 
+  backend "gcs" {
+    bucket = "feral-vision-operations-us-east4"
+    prefix = "terraform/cloud-smoke"
+  }
+
   required_providers {
     google = {
       source  = "hashicorp/google"
